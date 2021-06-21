1cf7f7440db9c458b1ea9b97d56c4097ded32b69
rajesh <rksahu.1214@gmail.com>
2021-06-21 18:04:24 +0530

    changes made

--- src/components/draggablebox.component.js
+++ src/components/draggablebox.component.js
@@ -1,24 +1,40 @@
 import DraggableItem from './draggableitem.component';
-import { useState,useRef } from 'react';
+import { useState,useRef,useCallback,useEffect } from 'react';
 import Draggable from 'react-draggable';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// import logo from './logo.svg';
+import '../App.css';
 function Draggablebox(props){  
+    
     const [boxesList,setBoxesList] = useState([]);
     const [boxes,setBoxes] =useState(0);
     const [renderedBoxes,setRenderedBoxes]=useState([]);
     function addBox(){
-        let newBox={ "id": boxes+1 , "name": boxes+1 };
+       
         let nboxesList=boxesList.slice();
+        let newBox={ "id": boxes+1 , "name": boxes+1,"position":{x:0,y:0}};
+        if(nboxesList.length>0){
+            let x=nboxesList[nboxesList.length-1].position.x - 0.5*nboxesList[nboxesList.length-1].position.x;
+            let y=nboxesList[nboxesList.length-1].position.y - 0.5*nboxesList[nboxesList.length-1].position.y;
+
+            newBox.position=nboxesList[nboxesList.length-1].position;
+        }
         nboxesList.push(newBox);
+
         setBoxes(boxes+1);
-        setBoxesList(nboxesList);
-        renderBoxes();
+        setBoxesList(nboxesList)
+        console.log(boxesList);
+       
     }
+
+    useEffect(() => {
+        renderBoxes();
+     }, [boxesList]);
     function renderBoxes(){
+       
         const newRenderedBoxes = boxesList.map((box) =>
-            <DraggableItem key={box.id} id={box.name} />
+            <DraggableItem key={box.id} id={box.name} position={box.position} boxesList={boxesList} setBoxesList={setBoxesList}/>
         );
         let temp="";let finalList="";
         for(let i=0;i<newRenderedBoxes.length;i++){
@@ -26,8 +42,9 @@ function Draggablebox(props){
                 temp=newRenderedBoxes[i];
                 finalList=temp;
             }else{
-                finalList=React.cloneElement(newRenderedBoxes[i],{ children: temp});
+                finalList=React.cloneElement(newRenderedBoxes[i],{children: temp});
                 temp=finalList;
+               
             }
         }
         setRenderedBoxes(finalList);
@@ -37,16 +54,22 @@ function Draggablebox(props){
     }
     return (
         
-        <div id="root">
-              <button onClick={(e)=>{addBox()}}>add a box</button>
-              <div id="window">
+        <div>
+        <h1>Draggable</h1>
+        <p>
+        <button onClick={(e)=>{addBox()}}>add a box</button>
+        </p>
+        <div className="box" style={{height: '1000px', width: '100%', position: 'relative', overflow: 'none', padding: '0'}}>
+        <div style={{height: '1000px', width: '100%', padding: '5px'}}>
+            
                   {
 
                         renderedBoxes
 
 
                   }
-              </div>
+        </div>
+        </div>
         </div>
     );
 }
--- src/components/draggableitem.component.js
+++ src/components/draggableitem.component.js
@@ -2,21 +2,73 @@ import Draggable from 'react-draggable';
 import { useState,useRef } from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// import logo from './logo.svg';
+import '../App.css';
 function DraggableItem(props){  
+    const [disabled,setDisabled] = useState(true);
+    const [position,setPosition] = useState(props.position);
+    const onStart = () => {
+        console.log("started");
+      };
+    
+    const onStop = () => {
+        console.log("stoped")
+        let nboxesList=props.boxesList.slice();
+        nboxesList[props.id-1].position=position;
+        props.setBoxesList(nboxesList);
+        console.log(nboxesList);
+        
+      };
+    const enableDrag = (e)=>{
+        console.log("diabled")
 
+        setDisabled(false);
+    }
+    const disableDrag = (e,ui)=>{
+       
+
+        // props.setLatestPosition(props.id,e.position);
+       
+        setDisabled(true);
+       
+    }
+    const handleDrag = (e, ui) => {
+        const {x, y} = position;
+        setPosition(
+          {
+            x: x + ui.deltaX,
+            y: y + ui.deltaY,
+          }
+        );
+       
+      };
+    const dragHandlers = {onStart: onStart,onDrag: handleDrag, onStop: onStop};
     return (
+  
+       
       
-        <div>
-            <Draggable>
-            <div className="Box">
-                {props.id}
-                <div>
-                    {props.children}
+          <Draggable bounds="parent" handle="strong" disabled={disabled} {...dragHandlers} defaultPosition={props.position}>
+            <div className="box no-cursor">
+    <div onMouseEnter={(e)=>enableDrag(e)} onMouseLeave={(e)=>disableDrag(e)}><strong className="cursor">Drag here {props.id}</strong></div>
+              I can only be moved within my Parent.<br /><br />
+              Both parent padding and child margin work properly.
+              <div className="box" style={{ width: '100%', position: 'relative', overflow: 'none', padding: '0'}}>
+                <div style={{ width: '100%', padding: '5px'}}>
+                    
+                        {
+
+                                props.children
+
+
+                        }
                 </div>
             </div>
-            </Draggable>
-        </div>
+            </div>
+           
+          </Draggable>
+            
+       
+        
     );
 }
 
